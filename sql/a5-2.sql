ba8a9aaf707a9ce2de8a3d82c1502628e78b51c5
civic <tsskmail@gmail.com>
2017-10-28 22:02:54 +0900

    fix 05-01

--- sql/a5-2.sql
+++ sql/a5-2.sql
@@ -1,9 +1,9 @@
-DROP TABLE IF EXISTS schedules;
-CREATE TABLE schedules(
-    name text primary key,
-    start_end int4range,
-    EXCLUDE USING gist (start_end WITH &&)
+DROP TABLE IF EXISTS circles;
+CREATE TABLE circles(
+    c circle,
+    EXCLUDE USING gist(c WITH &&)
 );
-
+INSERT INTO circles VALUES('<(0, 0), 5>');
+INSERT INTO circles VALUES('<(10, 0), 5>');
 
 
